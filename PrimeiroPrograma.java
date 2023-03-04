f3f8ebf9dce3141fad743d462dbab100cc828f08
Barretuz <112852499+Barretuz@users.noreply.github.com>
2023-03-04 03:41:56 -0300

    First Commit

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,24 @@
+package br.com.DIO;
+
+import br.com.DIO.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		Gato gato = new Gato();
+		
+		System.out.println(gato);
+		
+		/*int a = 2;
+		int b = 3;		
+		System.out.println("Hello world!! " + (a+b));*/
+	}
+	
+class Livro{
+	private String nome;
+	private Integer npag;
+}	
+
+}
+
